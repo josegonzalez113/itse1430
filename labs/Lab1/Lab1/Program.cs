78a9b78760a1a66a76c54d0bf2f1aa45543c0dc2
josegonzalez113 <jose.gonzalez113@my.tccd.edu>
2020-01-29 19:40:29 -0600

    New project lab1

---
+++ labs/Lab1/Lab1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Jose Gonzalez 
+// Class : ITSE 1430
+
+using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
